776eaea975b1ff70f18dc9673f3fbfd5fe295936
songbinliu <songbin.liu@turbonomic.com>
2018-04-06 16:37:06 -0400

    add k8s deployment for prometheus

--- start_prometheus.sh
+++ start_prometheus.sh
@@ -8,5 +8,5 @@ echo "$datav"
 options="-config.file=/prometheus-data/prometheus.yml"
 options="$options --storage.local.path=/prometheus-data/data/"
 
-docker run -d -p 9090:9090 -v $datav:/prometheus-data/ prom/prometheus  $options 
+docker run -d -p 19090:9090 -v $datav:/prometheus-data/ prom/prometheus  $options 
 # docker run -p 9090:9090  prom/prometheus  
